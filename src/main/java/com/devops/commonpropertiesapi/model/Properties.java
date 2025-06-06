7f810ff35dede5c1a73ae58b05c4aa0605a0f5c2
Merghoob Khan <merghoob.khan@siemens.com>
2025-06-06 16:17:12 +0530

    tep 3: Create the data model
    Properties class to represent the actual data
    @Data (Lombok): This powerful annotation from the Lombok library automatically generates all the boilerplate code (constructors, getters, setters, equals(), hashCode(), toString()) for your class. This makes your model code very clean and readable.
    @Document(collection = "properties"): This Spring Data MongoDB annotation tells the framework that this Property class should be mapped to a MongoDB collection named "properties". If the collection doesn't exist, MongoDB will create it automatically when the first document is inserted.
    @Id: This annotation designates the id field as the primary key within your MongoDB document. Spring Data MongoDB will automatically handle the generation and mapping of this ID.
    @CompoundIndex(def = "{'entity': 1, 'entityId': 1, 'key': 1}", unique = true): This is crucial for maintaining data integrity and improving performance.
    
        It tells MongoDB to create an index on the combination of entity, entityId, and key fields.
        unique = true ensures that you cannot have two property documents with the exact same entity, entityId, and key. For example, user-123 can only have one theme property. This prevents duplicate entries and enforces our logical model.
    
    entity, entityId, key, value: These are the core fields of our generic property model. Storing value as a String provides maximum flexibility, allowing you to store anything from simple booleans and numbers to complex JSON objects. The consuming application will be responsible for deserializing this string into the appropriate type.

---
+++ src/main/java/com/devops/commonpropertiesapi/model/Properties.java
@@ -0,0 +1,22 @@
+package com.devops.commonpropertiesapi.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "properties") // Spring data mongoDB: Maps this class to a MangoDB collection "properties"
+@CompoundIndex(def = "{'entity': 1, 'entityId': 1, 'key': 1}", unique = true) // ensure uniqueness of the combination
+public class Properties {
+
+	@Id   //Spring data MongoDB: mark the following fields as a primary key
+	private String id;  //MongoDB uses String IDs by default
+
+	private String entity;  // user, company, feature
+	private String entityId; // user-xyz, siemens, dark-toggle-button
+	private String key;  //"theme", "notificationsEnabled", "circuitBreakerState"
+	private String value; // the actual property value; can be stored as a string (can be JSON, boolean, number, etc.)
+
+
+}
